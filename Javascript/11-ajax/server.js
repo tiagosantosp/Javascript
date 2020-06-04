cefb6224d9d3bcef015214ea8634b560c147ee6c
Tiago Santos <tiago_s.p@hotmail.com>
2020-06-04 07:37:56 -0300

    ajax

---
+++ Javascript/11-ajax/server.js
@@ -0,0 +1,33 @@
+const bodyParser = require("body-parser")
+const express = require("express")
+
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({extended: true}))//se for formulario sera executado esse
+app.use(bodyParser.json())// se for JSON sera executado esse
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination : function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err =>{
+        if(err) {
+            return res.end("Ocorreu um erro")
+        }
+
+        res.end("CONCLUIDO COM SUCESO")
+    })
+})
+
+app.listen(8080, () => console.log('Executando...'))
