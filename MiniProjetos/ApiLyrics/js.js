c8dec6a30fd39b6f243a5e711d0cf016e7ea2b4d
tiagosantosp <tiago_s.p@hotmail.com>
2020-08-06 05:00:47 -0300

    apilyrics atualizado

--- MiniProjetos/ApiLyrics/js.js
+++ MiniProjetos/ApiLyrics/js.js
@@ -1,16 +1,14 @@
 
 const url =`https://api.lyrics.ovh/v1/Taylor Swift/cardigan`
-
-
-
-
+const p = document.getElementById('letra')
 
 fetch(url , { 
     method: 'get' // opcional 
   }).then(
     response => response.json()
   ).then(letra => {
-    console.log(typeof(letra.lyrics))
-    document.body.innerHTML = `<p>${letra.lyrics}</p>`
+    console.log(letra.lyrics)
+    p.innerHTML = letra.lyrics
+    p.style.textAlign = 'justify'
   })
 
