71bb579d5c32f8037ba0777ed291ed799b540863
Tiago Santos <tiago_s.p@hotmail.com>
2021-04-09 05:51:29 -0300

    Calculadora Vue

---
+++ Javascript/17-Vue/calculator/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+      <h1>Olá mundo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    @font-face {
+        font-family: "RobotoMono";
+        src: url("./fonts/RobotoMono-Thin.ttf");
+    }
+
+    * {
+        font-family: "RobotoMono", monospace;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+
+        color: #fff;
+        background: linear-gradient(to right,#9796f0, #fbc7d4);
+    }
+</style>
