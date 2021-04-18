d60c4b41d87fdf5e1d55f51512ebfe8cd9760861
Tiago Santos <tiago_s.p@hotmail.com>
2021-04-18 16:07:07 -0300

    Calculadora em VueJS

--- Javascript/17-Vue/calculator/src/App.vue
+++ Javascript/17-Vue/calculator/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
-      <h1>Olá mundo</h1>
+      <h1>Calculator</h1>
+      <Calculator />
   </div>
 </template>
 
 <script>
+import Calculator from './main/Calculator'
+
 export default {
+  components: { Calculator },
 
 }
 </script>
