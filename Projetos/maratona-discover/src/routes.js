2349bfc3bc03afbb40ebd546ef65ea355486d9aa
Tiago Santos <tiago_s.p@hotmail.com>
2021-05-25 06:09:09 -0300

    Jobs Calc

---
+++ Projetos/maratona-discover/src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/views/index.html')
+})
+
+routes.get('/index.html', (req, res) => {
+    return res.redirect('/')
+})
+module.exports = routes
---
+++ Projetos/maratona-discover/src/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+
+app.use(express.static("public"))
+
+app.use(routes)
+
+app.listen(3000, () => console.log("RODANDO..."))
