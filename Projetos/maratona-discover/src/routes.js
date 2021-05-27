69de6303f837f59432aab1f87bed05b64467fd61
Tiago Santos <tiago_s.p@hotmail.com>
2021-05-27 06:07:02 -0300

    rota post

--- Projetos/maratona-discover/src/routes.js
+++ Projetos/maratona-discover/src/routes.js
@@ -7,18 +7,51 @@ const basePath = __dirname + '/views/'
 
 const profile = {
     name: 'Tiago',
-    avatar: 'https://avatars.githubusercontent.com/u/49294650?v=4',
+    avatar: 'https://https://github.com/tiagosantosp.png',
     "monthly-budget" : 3000,
     "hours-per-day" : 5,
     "days-per-week" : 5,
     "vacation-per-year" : 4
-
 }
 
+jobs = [
+    {
+        id:  1,
+        name: "Pizzaria Guloso",
+        'daily-hours': 2, 
+        'total-hours': 60,
+        createdAt :  Date.now()
+    },
+    {
+        id:  2,
+        name: "OneTwo Project",
+        'daily-hours': 3, 
+        'total-hours': 47,
+        createdAt :  Date.now()
+    }
+]
+
 //ao utilizar o metodo render ele renderiza utilizando a engine 
-routes.get('/', (req, res) => res.render(basePath + 'index', {profile}))
-routes.get('/job', (req, res) => res.render(basePath + 'job', {profile}))
-routes.get('/job/edit', (req, res) => res.render(basePath + 'job-edit', {profile}))
+routes.get('/', (req, res) => res.render(basePath + 'index', {jobs}))
+
+routes.get('/job', (req, res) => res.render(basePath + 'job', ))
+
+routes.post('/job', (req, res) => {
+  
+    const lastId = jobs[jobs.length - 1]?.id || 1
+
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        'daily-hours': req.body["daily-hours"], 
+        'total-hours': req.body["total-hours"],
+        createdAt :  Date.now()
+    })
+    return res.redirect('/')
+})
+
+routes.get('/job/edit', (req, res) => res.render(basePath + 'job-edit', ))
+
 routes.get('/profile', (req, res) => res.render(basePath + 'profile', {profile}))
 
 module.exports = routes
--- Projetos/maratona-discover/src/server.js
+++ Projetos/maratona-discover/src/server.js
@@ -8,6 +8,9 @@ app.set('view engine', 'ejs')
 //ao colocar os arquivos estaticos na pasta public o express consegue acessar de qualquer lugar da aplicação
 app.use(express.static("public"))
 
+//habilitar o corpo da requisição
+app.use(express.urlencoded({extended: true}))
+
 app.use(routes)
 
 app.listen(3000, () => console.log("RODANDO..."))
