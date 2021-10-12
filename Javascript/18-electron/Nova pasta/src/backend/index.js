cc1a953641cd7fc7802f01fc0dc96ad01bab0685
Tiago Santos <tiago_s.p@hotmail.com>
2021-10-12 10:39:09 -0300

    Removendo Electron

--- Javascript/18-electron/Nova pasta/src/backend/index.js
+++
@@ -1,8 +0,0 @@
-const electron = window.require('electron');
-const ipcRenderer  = electron.ipcRenderer;
-
-ipcMain.on('canal' , (event, args) => {
-    console.log(args)
-
-    event.reply('canal', 'dados recebidos')
-})
